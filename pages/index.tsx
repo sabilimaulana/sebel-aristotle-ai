61adb9beaafbeff9d6cf9acf655ee0211f9ccaa6
sabilimaulana <spacysbl.learn@gmail.com>
2023-01-09 13:26:51 +0700

    feat: add in the OpenAI API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,39 @@
 import Head from "next/head";
 import Image from "next/image";
 import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    if (isGenerating) return;
+
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Sebel X Aristotle</title>
       </Head>
       <div className="container">
         <div className="header">
@@ -21,8 +48,42 @@ const Home = () => {
           </div>
         </div>
         <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" />
+          <textarea
+            placeholder="What's your favorite book?"
+            className="prompt-box"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <div className="prompt-buttons">
+            <button
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              disabled={isGenerating}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </button>
+          </div>
         </div>
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
 
       <div className="badge-container grow">
